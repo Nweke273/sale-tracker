b48860f6a38cd99989544b2991027a4651f351ec
Nweke273 <chisom5710@gmail.com>
2023-06-21 10:55:41 -0700

    feat: add more migration files

---
+++ app/Models/WorkFlowTodoAction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class WorkFlowTodoAction extends Model
+{
+    use HasFactory;
+}
